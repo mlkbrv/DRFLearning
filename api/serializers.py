1fddf448de95a26d35b189d11fdd83f1167a2a2c
E <emilakbarov857@gmail.com>
2025-07-17 01:07:49 +0400

    Initial commit

---
+++ api/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from .models import Product, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'name',
+            'description',
+            'price',
+            'stock',
+        ]
+
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError('Price cannot be negative')
+        return value
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(source='product.price',max_digits=10, decimal_places=2)
+    class Meta:
+        model = OrderItem
+        fields = [
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal'
+        ]
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True,read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        items = obj.items.all()
+        return sum(item.item_subtotal for item in items)
+
+    class Meta:
+        model = Order
+        fields = [
+            'order_id',
+            'user',
+            'created_at',
+            'status',
+            'items',
+            'total_price'
+        ]
+
+class ProductInfoSerializer(serializers.Serializer):
+    #get all products,count of products,max price
+    products = ProductSerializer(many=True,read_only=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
---
+++ api/views.py
@@ -0,0 +1,61 @@
+from django.db.models import Max
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+
+# @api_view(['GET'])
+# def product_list(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True)
+#     return Response(serializer.data)
+        # JsonResponse({'data': serializer.data})
+
+class ProductsListAPIView(generics.ListAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+
+
+# @api_view(['GET'])
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+
+# @api_view(['GET'])
+# def order_list(request):
+#     orders = Order.objects.prefetch_related('items__product').all()
+#     serializer = OrderSerializer(orders, many=True)
+#     return Response(serializer.data)
+
+class OrdersListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+
+class UserOrdersListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related('items__product')
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        qs= super().get_queryset()
+        return qs.filter(user=user)
+
+
+@api_view(['GET'])
+def product_info(request):
+    products = Product.objects.all()
+    serializer = ProductInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']
+    })
+    return Response(serializer.data)
